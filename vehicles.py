fedbb6c985c6f7e393388ef6f20dc7b087fce98f
JieDh <jie.dh13@gmail.com>
2021-01-04 19:12:07 +0100

    vehicles.py changed

--- vehicles.py
+++ vehicles.py
@@ -2,4 +2,17 @@ import pandas as pd
 def df_shape(x):
     return x.shape
 vehicles = pd.read_csv('data/vehicles.csv', sep=',')
-print(df_shape(vehicles))
+print("Dataframe shape:", df_shape(vehicles))
+
+def df_rows(x):
+        return len(x)
+print("Dataframe rows:", df_rows(vehicles))
+
+def df_cols(x):
+        return len(x.columns)
+print("Dataframe columns:", df_cols(vehicles))
+
+def df_col_name(x):
+	return x.columns.to_list()
+print("Column names:", df_col_name(vehicles))
+
